e756f6509f7920aa6f139390b23621c29c20bdf8
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-07-12 11:55:09 +0530

    Changed Home Folder, Add proFile Image and Cloudinary uploader and logout controller

---
+++ EcommerceWebsite/server/routes/auth.router.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import {
+    registerController,
+    loginController,
+    logoutController,
+    changePasswordController,
+    userProfileController
+} from '../controllers/auth.controller.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+import { upload } from '../middlewares/multer.middleware.js'
+
+const router = express.Router()
+
+
+
+router.route('/register').post(upload.fields([
+    {
+        name: 'profileImage',
+        maxCount: 1
+    }
+]), registerController)
+
+router.route('/login').post(loginController)
+
+
+
+
+// Secured Routes
+router.route('/logout').post(verifyJWT, logoutController)
+
+router.route('/change-password').patch(verifyJWT, changePasswordController)
+
+router.route('/user-profile').get(verifyJWT, userProfileController)
+
+
+export default router
