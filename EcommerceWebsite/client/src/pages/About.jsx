e58bf470ff48871038037052c6b6955926f7bddb
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-07-15 13:21:13 +0530

    Created nice About page

--- EcommerceWebsite/client/src/pages/About.jsx
+++ EcommerceWebsite/client/src/pages/About.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import '../styles/about.css'
+import React from "react";
+import "../styles/about.css";
+import { Typography } from "@mui/material";
 
 function About() {
   return (
     <>
-    <div className="container">
-      <div className="about-box">
-        <h1 className="about-title">About Us</h1>
-        <p className="about-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi mollitia saepe ipsum voluptas incidunt expedita accusamus nostrum reiciendis quod nobis optio distinctio, consectetur earum maxime, eaque excepturi neque, vero doloremque nesciunt alias. Porro earum sunt repudiandae quod non dolorem ullam odio reiciendis, ea dignissimos sint exercitationem error, ex aut maiores labore quasi cumque voluptates impedit odit voluptas. Blanditiis placeat, harum beatae itaque obcaecati illo fugiat ad doloribus autem tempore ut nihil exercitationem magnam repellendus? Corporis omnis impedit fugiat! </p>
+      <div className="about-container">
+        <div className="about-box">
+          <Typography variant="h1" sx={{
+            fontSize: '3rem', fontWeight: '500'
+          }} className="about-title">About Us</Typography>
+          <p className="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            mollitia saepe ipsum voluptas incidunt expedita accusamus nostrum
+            reiciendis quod nobis optio distinctio, consectetur earum maxime,
+            eaque excepturi neque, vero doloremque nesciunt alias. Porro earum
+            sunt repudiandae quod non dolorem ullam odio reiciendis, ea
+            dignissimos sint exercitationem error, ex aut maiores labore quasi
+            cumque voluptates impedit odit voluptas.{" "}
+          </p>
+        </div>
+        <div className="about-img-container">
+          {/* <img src="\image1 (2).png" alt="image" className="about-image" /> */}
+          <img src="/image3wisperingwillow.png" alt="image" className="about-image" />
+        </div>
       </div>
-    </div>
     </>
-  )
+    
+  );
 }
 
-export default About
+export default About;
