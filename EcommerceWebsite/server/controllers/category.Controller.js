f2c7db117577389b14464f9fe62989e58afb786d
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-07-16 17:13:14 +0530

    Added Create-Category Controller

--- EcommerceWebsite/server/controllers/category.Controller.js
+++ EcommerceWebsite/server/controllers/category.Controller.js
@@ -1,7 +1,50 @@
-import categoryModel from '../models/category.model.js'
+import CategoryModel from '../models/category.model.js'
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import slugify from 'slugify'
 
 
-export const getAllCategoriesController = async(req, res)=>{
-    
+export const createCategoryController = async (req, res) => {
+    try {
+        const { name } = req.body
+
+        if (!name) {
+            return res.status(400).json({ message: 'Name is required!' })
+        }
+
+        const categoryExists = await CategoryModel.findOne({ name })
+
+        if (categoryExists) {
+            return res.status(400).json({
+                success: false,
+                message: 'Category already exists!'
+            })
+        }
+
+        const category = await CategoryModel.create({ name, slug: slugify(name) })
+
+        return res.status(201).json({
+            success: true,
+            message: 'Category created successfully!',
+            category
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            success: false,
+            message: 'Something went wrong in createCategoryController'
+        })
+    }
+}
+
+
+export const getAllCategoriesController = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            success: false,
+            message: 'Something went wrong in getAllCategoriesController'
+        })
+    }
 }
