97875fb717a5db2d5959ea8a80606fcd11657851
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-07-24 21:55:46 +0530

    Added Images to folder

--- EcommerceWebsite/client/src/pages/HomePage.jsx
+++ EcommerceWebsite/client/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../styles/homePage.css'
 
 function HomePage() {
   return (
     <>
-      <h1            >Home page</h1>
+      <div className="home-container">
+        <section className="home-hero-section">
+          <h1 className="home-hero-title">Whispering Willow</h1>
+          <p className="home-hero-p1">Embrace Nature’s Best for Your Hair & Skin</p>
+          <img src="/homePage/herosectionnightimage2.png" alt="Image" className="home-hero-image" />
+          <p className="home-hero-text"></p>
+        </section>
+      </div>
     </>
   )
 }
