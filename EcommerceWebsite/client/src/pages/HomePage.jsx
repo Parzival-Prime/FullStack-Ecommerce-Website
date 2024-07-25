b92a4d992cccbdde31bf93f654400bd1fe197a87
Divyanshu Rajput <dvnhrajput@gmail.com>
2024-07-26 02:12:00 +0530

    For time being until I am satisfir=ed with functionality and learned more for design and frontend

--- EcommerceWebsite/client/src/pages/HomePage.jsx
+++ EcommerceWebsite/client/src/pages/HomePage.jsx
@@ -8,8 +8,9 @@ function HomePage() {
         <section className="home-hero-section">
           <h1 className="home-hero-title">Whispering Willow</h1>
           <p className="home-hero-p1">Embrace Nature’s Best for Your Hair & Skin</p>
-          <img src="/homePage/herosectionnightimage2.png" alt="Image" className="home-hero-image" />
-          <p className="home-hero-text"></p>
+          <img src="\homePage\temphd.png" alt="Image" className="home-hero-image" />
+          <p className="home-hero-text">Nourish your hair and skin with our range of all-natural, high-quality products. Crafted with care, inspired by nature.</p>
+          <button className="home-hero-button">Explore Products</button>
         </section>
       </div>
     </>
